a84b57be63a408ae9454a98b0e04b0d62de3fccc
AnshulRokde17 <110658352+AnshulRokde17@users.noreply.github.com>
2022-08-05 12:43:12 +0530

    Add files via upload

---
+++ SonarDemoApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SonarDemoApp
+//
+//  Created by Anshul Rokade on 04/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
